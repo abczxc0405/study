00c88539ab8bdb0b03e9e382abd409d9bdbc114e
JunyongPark <114290913+abczxc0405@users.noreply.github.com>
2022-11-29 21:07:48 +0900

    문자열을 화면에 출력하는 프로그램

---
+++ c/2-1.c
@@ -0,0 +1,10 @@
+// 문자열을 화면에 출력하는 프로그램
+
+#include <stdio.h>  // stdio : standard input output(표준입출력)의 줄임말
+
+int main(void) {
+    printf("Hello");    // 문자열 "Hello" 출력
+    printf("World!");   // 문자열 "World!" 출력
+
+    return 0;
+}
